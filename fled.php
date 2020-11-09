fc6dc666b4d8ca1bf7cb8be82e0b73b525da9401
herix1 <74185748+herix1@users.noreply.github.com>
2020-11-09 21:23:41 +0100

    jj

--- fled.php
+++ fled.php
@@ -1,4 +1,4 @@
-{"GlobalCanliTvPro":[
+{"Mobilim":[
 {"ads_tip":"Admob",
 "main_link":"https://github.com/herix0/herix0.github.io/raw/master/app/pays.php",
 "app_id":"ca-app-pub-9133302355759736~1528781501",
