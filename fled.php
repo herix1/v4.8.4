717518871d46dd0ffc154335162a8dcbac97c96a
herix1 <74185748+herix1@users.noreply.github.com>
2020-11-16 20:20:07 +0100

    jn

--- fled.php
+++ fled.php
@@ -1,8 +1,8 @@
 {"GlobalCanliTvPro":[
 {"ads_tip":"Admob",
 "main_link":"https://github.com/herix0/herix0.github.io/raw/master/app/pays.php",
-"app_id":"ca-app-pub-9133302355759736~1528781501",
-"banner_unit_id":"ca-app-pub-9133302355759736/7084182901",
-"inter_unit_id":"ca-app-pub-9133302355759736/1831856222",
+"app_id":"ca-app-pub-8924166356296470~5734364219",
+"banner_unit_id":"ca-app-pub-8924166356296470/2455456570",
+"inter_unit_id":"ca-app-pub-8924166356296470/1142374900",
 "iron_id":""},
 ]}
